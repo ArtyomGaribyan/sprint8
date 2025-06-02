b2539c5b93ec1878615bba08eab50d014f2bccf4
Artyom Garibyan <artyomgaribyan0@gmail.com>
2025-06-02 09:16:45 +0400

    third commit

--- parcel_test.go
+++ parcel_test.go
@@ -50,7 +50,11 @@ func TestAddGetDelete(t *testing.T) {
 	// проверьте, что значения всех полей в полученном объекте совпадают со значениями полей в переменной parcel
 	p, err := store.Get(id)
 	assert.NoError(t, err)
-	assert.Equal(t, parcel, p)
+	assert.Equal(t, parcel.Address, p.Address)
+	assert.Equal(t, parcel.Client, p.Client)
+	assert.Equal(t, parcel.CreatedAt, p.CreatedAt)
+	assert.Equal(t, parcel.Status, p.Status)
+	assert.Equal(t, id, p.Number)
 
 	// delete
 	// удалите добавленную посылку, убедитесь в отсутствии ошибки
